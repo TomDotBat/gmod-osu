ade36a88c4d399a3ccaa7e49cd43b9812e7e6056
Tom <tom@tomdotbat.dev>
2020-08-30 22:12:00 +0100

    Approach circles and circle shadows

--- lua/tom_osu/vgui/cl_frame.lua
+++ lua/tom_osu/vgui/cl_frame.lua
@@ -14,27 +14,34 @@ function PANEL:Init()
         end)
     end
 
-    timer.Simple(0, function()
-        surface.PlaySound("../data/gmod-osu/song1.mp3")
-    end)
+    --surface.PlaySound("../data/gmod-osu/music.mp3")
 
     self.Circles = {}
+    self.Shadows = {}
+    self.HitCount = 0
 end
 
-function PANEL:OsuPixelToReal(x, y)
-
-end
-
-local circleSize = 32
+local curTime = UnPredictedCurTime
 
 function PANEL:SpawnCircle(x, y)
     local circle = vgui.Create("osu.circle", self)
 
-    circle:SetSize(circleSize, circleSize)
-    circle:SetPos(x - circleSize / 2, y - circleSize / 2)
+    circle:SetFullApproachTime(curTime() + .25)
 
-    --circle:SetAlpha(0)
+    circle:SetSize(self.CircleSize, self.CircleSize)
+
+    local halfCircle = self.CircleSize / 2
+    circle:SetPos(self.PlayAreaBounds[1] + (osu.osuPixelsToReal(x) - halfCircle), self.PlayAreaBounds[2] + (osu.osuPixelsToReal(y) - halfCircle))
+
     circle:AlphaTo(255, .25)
+
+    circle.DoClick = function(s)
+        osu.log("Hit")
+        self.HitCount = self.HitCount + 1
+
+        s:Remove()
+        self:AddShadow(s.x, s.y, s.Color)
+    end
 
     timer.Simple(.25, function()
         if not IsValid(circle) then return end
@@ -47,8 +54,41 @@ function PANEL:SpawnCircle(x, y)
     table.insert(self.Circles, circle)
 end
 
+function PANEL:AddShadow(x, y, col)
+    table.insert(self.Shadows, {
+        x = x,
+        y = y,
+        color = color_white,
+        num = 1,
+        alpha = 1
+    })
+end
+
+function PANEL:PerformLayout(w, h)
+    self.CircleSize = osu.osuPixelsToReal(osu.getCircleSize(5))
+    self.PlayAreaBounds = {osu.getPlayAreaBounds()}
+end
+
+local lerp = Lerp
+
+local playAreaCol = Color(20, 20, 20)
+
 function PANEL:Paint(w, h)
     draw.RoundedBox(0, 0, 0, w, h, color_black)
+    draw.RoundedBox(0, self.PlayAreaBounds[1], self.PlayAreaBounds[2], self.PlayAreaBounds[3], self.PlayAreaBounds[4], playAreaCol)
+
+    for k,v in ipairs(self.Shadows) do
+        surface.SetAlphaMultiplier(v.alpha)
+        draw.RoundedBox(self.CircleSize, v.x, v.y, self.CircleSize, self.CircleSize, v.color)
+
+        v.alpha = lerp(FrameTime(), v.alpha, 0)
+        if v.alpha > .001 then table.remove(self.Shadows, k) end
+    end
+
+    surface.SetAlphaMultiplier(1)
+
+
+    draw.SimpleText(self.HitCount, "DermaLarge", 0, 0, color_white)
 end
 
 vgui.Register("osu.frame", PANEL, "EditablePanel")
--- lua/tom_osu/vgui/game_objects/cl_circle.lua
+++ lua/tom_osu/vgui/game_objects/cl_circle.lua
@@ -1,14 +1,38 @@
 
 local PANEL = {}
 
+AccessorFunc(PANEL, "nFullyApproachedTime", "FullyApproachedTime", FORCE_NUMBER)
+
 function PANEL:Init()
     self:SetIsCircular(true)
+    self:NoClipping(true)
 
     self.Color = ColorRand()
 end
 
+local curTime = UnPredictedCurTime
+
+function PANEL:SetFullApproachTime(time)
+    self.nFullyApproachedTime = time
+    self.nApproachTime = time - curTime()
+end
+
+function PANEL:GetApproachProgress()
+    return (self.nFullyApproachedTime - curTime()) / self.nApproachTime
+end
+
 function PANEL:Paint(w, h)
-    draw.RoundedBox(w, 0, 0, w, w, self.Color)
+    local approachProg = self:GetApproachProgress()
+    print(approachProg)
+    if approachProg > 0 then
+        local center = h / 2
+        draw.NoTexture()
+        --draw.Arc(center, center, center + approachProg * h, osu.osuPixelsToReal(2), 0, 360, 10, color_white)
+        --osu.drawUncachedArc(center, center, approachProg * h * 2, osu.osuPixelsToReal(2), 0, 360, 10, color_white)
+        print("drawing")
+    end
+
+    draw.RoundedBox(h, 0, 0, h, h, self.Color)
 end
 
 vgui.Register("osu.circle", PANEL, "osu.button")
